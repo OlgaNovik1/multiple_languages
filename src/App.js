1447cd4db5a6f3231f3224c99780e6881e5b3de1
OlgaNovik1 <7755517@gmail.com>
2023-07-21 16:37:45 +0300

    data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import { Suspense } from 'react'
-// import './i18n'
+import { Suspense, useState } from 'react'
 import './App.css';
 import { useTranslation } from 'react-i18next';
 
+const locales = {
+  en: { title: 'English' },
+  ru: { title: 'Russian' },
+}
+
 function App() {
+  const [messages, setMessages] = useState(0);
   const { t, i18n } = useTranslation()
 
 
   return (
     <div className="App">
+      <ul>
+        {Object.keys(locales).map((locale) => (
+          <li key={locale}>
+            <button
+              type='submit'
+              onClick={() => i18n.changeLanguage(locale)}
+              style={{ fontWeight: i18n.resolvedLanguage === locale ? 'bold' : 'normal' }}>
+              {locales[locale].title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
       <h1>{t('main.header')}</h1>
+
+      <button onClick={() => setMessages(messages + 1)}>+1 message</button>
+      <p>
+        {t('main.new_messages', { count: messages })}
+      </p>
+
+      <p>
+        {t('main.current_date', { date: new Date() })}
+      </p>
+
+      <p>
+        {t('main.incoming_message', { from: 'Alex' })}<br />
+        {t('main.message_contents', { body: 'How are you doing?', context: 'male' })}
+      </p>
+
+      {/* <h1>{t('ola')}</h1>   */}
+
     </div>
   );
 }
--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
+import { DateTime } from 'luxon'
 
 i18n
     .use(Backend)
@@ -9,9 +10,17 @@ i18n
     .use(initReactI18next)
     .init({
         debug: true,
-        fallbackLng: 'en'
+        fallbackLng: 'en',
+        returnEmptyString: false,
     });
+//для разныз языков разные форматы
+i18n.services.formatter.add('DATE_LONG', (value, lng, _options) => {
+    return DateTime.fromJSDate(value).setLocale(lng).toLocaleString(DateTime.DATE_HUGE)
+});
+i18n.services.formatter.add('DATE_SHOT', (value, lng, _options) => {
+    return DateTime.fromJSDate(value).setLocale(lng).toLocaleString(DateTime.DATE_SHORT)
+})
 
-i18n.changeLanguage('en') //быстро проверить перевод
+// i18n.changeLanguage('en') //быстро проверить перевод
 
 export default i18n;
